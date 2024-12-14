26795e542885c9533ded03c37c934998b34f4b15
Mahesh3work <mahesh3work@gmail.com>
2024-12-14 19:17:15 +0530

    by using axio with external api

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Posts } from "./components/Post";
+
+const App = () => {
+  return (
+    <section className="main-section">
+      <Posts/>
+    </section>
+  );
+};
+
+export default App;
